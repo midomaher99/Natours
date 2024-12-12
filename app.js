59744bc44b6ac56f5e58f16c2749572089832b6e
unknown <mohamed.mostafa.maher.999@gmail.com>
2024-12-13 00:29:14 +0200

    create new Tour route handler

--- app.js
+++ app.js
@@ -4,9 +4,13 @@ const fs = require('fs')
 const app = express();
 const port = 3000;
 
+//middlewares
+app.use(express.json());
+
 //reading tours data in js object
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
 
+//Routes handlers
 app.get("/api/v1/tours", (req, res) => {
     res
         .status(200)
@@ -17,13 +21,26 @@ app.get("/api/v1/tours", (req, res) => {
                 tours
             }
         });
-})
+});
 
-app.post("/", (req, res) => {
-    res
-        .status(200)
-        .send("you can post to this endpoint..")
-})
+app.post("/api/v1/tours", (req, res) => {
+    const newId = tours[tours.length - 1].id + 1;
+    const newTour = Object.assign({ id: newId }, req.body);
+    tours.push(newTour);
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), (err) => {
+        if (err) {
+            res.status(500).send("Server Error");
+        } else {
+            res.status(201).json({
+                status: 'success',
+                data: {
+                    newTour
+                }
+            });
+        }
+    })
+});
+
 
 
 app.listen(port, () => {
