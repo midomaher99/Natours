7d13a34c6e0014953d85812a4254219db5a9715c
unknown <mohamed.mostafa.maher.999@gmail.com>
2024-12-13 00:56:37 +0200

    Getting Tour by ID Route

--- app.js
+++ app.js
@@ -23,20 +23,47 @@ app.get("/api/v1/tours", (req, res) => {
         });
 });
 
+app.get("/api/v1/tours/:id", (req, res) => {
+    const id = req.params.id * 1;   //convert id from string to a number
+
+    const tour = tours.find((el) => { return el.id === id })
+
+    if (tour === undefined) {
+        res
+            .status(400)
+            .json({
+                status: "fail",
+                message: "Invalid ID"
+            })
+    } else {
+        res
+            .json({
+                status: "success",
+                data: {
+                    tour
+                }
+            })
+    }
+});
+
 app.post("/api/v1/tours", (req, res) => {
     const newId = tours[tours.length - 1].id + 1;
     const newTour = Object.assign({ id: newId }, req.body);
     tours.push(newTour);
     fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), (err) => {
         if (err) {
-            res.status(500).send("Server Error");
+            res
+                .status(500)
+                .send("Server Error");
         } else {
-            res.status(201).json({
-                status: 'success',
-                data: {
-                    newTour
-                }
-            });
+            res
+                .status(201)
+                .json({
+                    status: 'success',
+                    data: {
+                        newTour
+                    }
+                });
         }
     })
 });
