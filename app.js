9e152bd07e423dfa22b2277aa60758aafeafeccf
unknown <mohamed.mostafa.maher.999@gmail.com>
2024-12-14 23:11:36 +0200

    handling Patch and delete verbs

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ const port = 3000;
 app.use(express.json());
 
 //reading tours data in js object
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+const toursSimplePath = `${__dirname}/dev-data/data/tours-simple.json`;
+
+const tours = JSON.parse(fs.readFileSync(toursSimplePath));
 
 //Routes handlers
 app.get("/api/v1/tours", (req, res) => {
@@ -25,7 +27,6 @@ app.get("/api/v1/tours", (req, res) => {
 
 app.get("/api/v1/tours/:id", (req, res) => {
     const id = req.params.id * 1;   //convert id from string to a number
-
     const tour = tours.find((el) => { return el.id === id })
 
     if (tour === undefined) {
@@ -50,7 +51,7 @@ app.post("/api/v1/tours", (req, res) => {
     const newId = tours[tours.length - 1].id + 1;
     const newTour = Object.assign({ id: newId }, req.body);
     tours.push(newTour);
-    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), (err) => {
+    fs.writeFile(toursSimplePath, JSON.stringify(tours), (err) => {
         if (err) {
             res
                 .status(500)
@@ -68,7 +69,72 @@ app.post("/api/v1/tours", (req, res) => {
     })
 });
 
+app.patch("/api/v1/tours/:id", (req, res) => {
+    const id = req.params.id * 1;   //convert id from string to a number
+    const tour = tours.find((el) => { return el.id === id })
 
+    if (tour === undefined) {
+        res
+            .status(400)
+            .json({
+                status: "fail",
+                message: "Invalid ID"
+            })
+    } else {
+        Object.assign(tour, req.body);
+        let tourId = tours.findIndex((el) => { return el.id === tour.id });
+        tours[tourId] = tour;
+        fs.writeFile(toursSimplePath, JSON.stringify(tours), (err) => {
+            if (err) {
+                console.log(err.message)
+                res
+                    .status(500)
+                    .send("Server Error");
+            } else {
+                res
+                    .status(200)
+                    .json({
+                        status: "success",
+                        data: {
+                            tour
+                        }
+                    })
+            }
+        })
+    }
+});
+
+app.delete("/api/v1/tours/:id", (req, res) => {
+    const id = req.params.id * 1;   //convert id from string to a number
+    let tourId = tours.findIndex((el) => { return el.id === id });
+
+    if (tourId === -1) {
+        res
+            .status(400)
+            .json({
+                status: "fail",
+                message: "Invalid ID"
+            })
+    } else {
+        tours.splice(tourId, 1);
+        fs.writeFile(toursSimplePath, JSON.stringify(tours), (err) => {
+            if (err) {
+                console.log(err.message)
+                res
+                    .status(500)
+                    .send("Server Error");
+            } else {
+                res
+                    .status(204)
+                    .json({
+                        status: "success",
+                        data: null
+                    })
+            }
+        })
+
+    }
+})
 
 app.listen(port, () => {
     console.log(`app is running on port:${port}`)
