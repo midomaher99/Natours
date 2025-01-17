ef03bb4aa95c13a68a0baa155f2cceef5ce135e9
unknown <mohamed.mostafa.maher.999@gmail.com>
2025-01-17 23:53:51 +0200

    adding successfull bookings into DB

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -4,7 +4,7 @@ const stripe = require('stripe')(process.env.STRIPE_SECRETE_KEY)
 const catchAsync = require(`${__dirname}/../utils/catchAsync`);
 const appError = require(`${__dirname}/../utils/appError`);
 const Tour = require(`${__dirname}/../models/tourModel`);
-const factory = require(`${__dirname}/handlerFactory`);
+const Booking = require(`${__dirname}/../models/bookingModel`);
 
 module.exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     //get the tour from DB
@@ -17,20 +17,10 @@ module.exports.getCheckoutSession = catchAsync(async (req, res, next) => {
     const session = await stripe.checkout.sessions.create({
         payment_method_types: ['card'],
         mode: 'payment',
-        success_url: `${req.protocol}://${req.get('host')}/`,
+        success_url: `${req.protocol}://${req.get('host')}/?tour=${req.params.tourId}&user=${req.user.id}&price=${tour.price}`,
         cancel_url: `${req.protocol}://${req.get('host')}/tour/${tour.slug}`,
         customer_email: req.user.email,
         client_reference_id: req.params.tourId,
-        // line_items: [
-        //     {
-        //         name: `${tour.name} Tour`,
-        //         description: tour.summary,
-        //         images: [`https://natours.dev/img/tours/${tour.imageCover}`],
-        //         amount: tour.price * 100,
-        //         currency: 'usd',
-        //         quantity: 1
-        //     }
-        // ]
         line_items: [
             {
                 price_data: {
@@ -38,15 +28,16 @@ module.exports.getCheckoutSession = catchAsync(async (req, res, next) => {
                     product_data: {
                         name: `${tour.name} Tour`,
                         description: tour.summary,
-                        images: [`https://natours.dev/img/tours/${tour.imageCover}`],
+                        images: [`https://natours.dev/img/tours/${tour.imageCover}`]
                     },
                     unit_amount: tour.price * 100, // Amount in cents
                 },
                 quantity: 1,
-            },
+            }
         ]
+    },
+    )
 
-    })
     //send to the client
     res
         .status(200)
@@ -55,3 +46,12 @@ module.exports.getCheckoutSession = catchAsync(async (req, res, next) => {
             session
         })
 });
+
+module.exports.createBookingCheckout = catchAsync(async (req, res, next) => {
+    const { tour, user, price } = req.query;
+    if (!tour || !user || !price) return next();
+
+    await Booking.create({ tour, user, price });
+
+    res.redirect(req.originalUrl.split('?')[0]);//removing booking data from query string
+})
--- routes/viewRouter.js
+++ routes/viewRouter.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const viewController = require(`${__dirname}/../controllers/viewController`);
 const authController = require(`${__dirname}/../controllers/authController`)
+const bookingController = require(`${__dirname}/../controllers/bookingController`)
 const router = express.Router();
 
 router.get('/me', authController.isLoggedIn, viewController.getAccount);
 router.post('/submit-user-data', authController.isLoggedIn, viewController.updateUserData);
 
 router.use(viewController.isLoggedIn);
-router.get('/', viewController.getOverview);
+router.get('/', bookingController.createBookingCheckout, viewController.getOverview);
 router.get('/tour/:slug', viewController.getTour);
 router.get('/login', viewController.getLoginForm);
 
